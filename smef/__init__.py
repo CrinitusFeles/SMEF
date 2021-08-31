05a3ff8d417e52e053f9bf42fb5dacf35f50cb8b
crinitusfeles <crinitusfeles@gmail.com>
2021-08-31 14:21:13 +0700

    fix: test stable release

--- smef/__init__.py
+++ smef/__init__.py
@@ -28,23 +28,24 @@ except Exception as ex:
     from app_logger import *
 
 # there is problem with version control: qdarkstyle requires python >3.6, but AstraLinux has only 3.5
-import pip
-import importlib.util
-
-def install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
-
-def upgrade(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package, '--upgrade'])
-    else:
-        pip._internal.main(['install', package, '--upgrade'])
-
-spec = importlib.util.find_spec('qdarkstyle')
-if spec is None:
-    print("qdarkstyle is not installed")
-    install('qdarkstyle')
-upgrade('pyqtgraph')
+# UPDATE: have assembled python3.9 for AstraLinux
+# import pip
+# import importlib.util
+#
+# def install(package):
+#     if hasattr(pip, 'main'):
+#         pip.main(['install', package])
+#     else:
+#         pip._internal.main(['install', package])
+#
+# def upgrade(package):
+#     if hasattr(pip, 'main'):
+#         pip.main(['install', package, '--upgrade'])
+#     else:
+#         pip._internal.main(['install', package, '--upgrade'])
+#
+# spec = importlib.util.find_spec('qdarkstyle')
+# if spec is None:
+#     print("qdarkstyle is not installed")
+#     install('qdarkstyle')
+# upgrade('pyqtgraph')
