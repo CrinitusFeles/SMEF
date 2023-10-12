86e401e22bc743db4ed47965081c508d8204c3e4
CrinitusFeles <crinitusfeles@gmail.com>
2023-10-12 13:53:21 +0700

    fixed plotter probe label

--- smef/client/custom_plot/CustomPlot.py
+++ smef/client/custom_plot/CustomPlot.py
@@ -93,7 +93,8 @@ class CustomPlot(QWidget):
                 index = np.where(ticks.astype(int) == int(mouse_point.x()))[0]
                 if len(index) > 0:
                     label: str = data_line.name()#.split()[-1]
-                    marker_text += f'{self.plotter_style.labels.marker}{label}: {data[index[0]]:.2f}\n'
+                    # marker_text += f'{self.plotter_style.labels.marker}{label}: {data[index[0]]:.2f}\n'
+                    marker_text += f'{label}: {data[index[0]]:.2f}\n'
         marker_text = marker_text[:-1]
         self.plotter_style.marker_label.setText(marker_text)
         self.plotter_style.marker_label.setPos(mouse_point)
